ad1c096623cbddfcc2928fae156b92204225be93
Distinct Hospitality <VERONICA@DISTINCTHOSPITALITYSOLUTIONS.COM>
2024-12-03 19:58:29 +0100

    Update admin.py

--- pages/admin.py
+++ pages/admin.py
@@ -17,19 +17,19 @@ st.write("")
 st.write("")
 
 #First datatable
-row1 = row([0.1, 0.9], vertical_align="center")
-row1.subheader("Roles")
-dfDatos_rol = cargarDatos('select * from Roles')
-row1.dataframe(dfDatos_rol, use_container_width=True)
+#row1 = row([0.1, 0.9], vertical_align="center")
+#row1.subheader("Roles")
+#dfDatos_rol = cargarDatos('select * from Roles')
+#row1.dataframe(dfDatos_rol, use_container_width=True)
 
 #Second datatable
-row2 = row([0.1, 0.9], vertical_align="center")
-row2.subheader("Users", anchor=False)
-dfDatos_usr = cargarDatos('select * from users')
-row2.dataframe(dfDatos_usr, use_container_width=True)
+#row2 = row([0.1, 0.9], vertical_align="center")
+#row2.subheader("Users", anchor=False)
+#dfDatos_usr = cargarDatos('select * from users')
+#row2.dataframe(dfDatos_usr, use_container_width=True)
 
 #third datatable
-row3 = row([0.4, 0.6], vertical_align="center")
-row3.subheader("Users - Roles ", anchor=False)
-dfDatos_usr = cargarDatos('select * from user_roles')
-row3.dataframe(dfDatos_usr, use_container_width=True)
+#row3 = row([0.4, 0.6], vertical_align="center")
+#row3.subheader("Users - Roles ", anchor=False)
+#dfDatos_usr = cargarDatos('select * from user_roles')
+#row3.dataframe(dfDatos_usr, use_container_width=True)
